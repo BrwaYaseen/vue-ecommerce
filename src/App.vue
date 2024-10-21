e4c9120dac3d12f94f4bb03be646315d822c5e6f
Brwa Yaseen <brwayasin29@gmail.com>
2024-10-21 16:53:51 +0300

    products

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script setup>
-import StoreFront from './components/StoreFront.vue'
-</script>
-
 <template>
-  <StoreFront />
+  <div id="app">
+    <NavBar :store-name="storeName" :cart="cart" />
+    <ProductsPage />
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import NavBar from './components/NavBar.vue'
+import ProductsPage from './components/ProductsPage.vue'
+
+const storeName = ref('Vue Boutique')
+const cart = ref([])
+</script>
