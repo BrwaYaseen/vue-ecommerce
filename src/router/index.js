9385ffc6d5bd7b8718f03f377d8800576bbfcf81
Brwa Yaseen <brwayasin29@gmail.com>
2024-10-28 18:11:13 +0300

    help page

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ProductsPage from '../views/ProductsPage.vue'
 import HomeView from '@/views/HomeView.vue'
 import CheckoutPage from '@/views/CheckoutPage.vue'
-import AboutUs from '@/views/AboutUs.vue'
+import HelpCenter from '@/views/HelpCenter.vue'
 
 const routes = [
   {
@@ -21,9 +21,9 @@ const routes = [
     component: CheckoutPage,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: AboutUs,
+    path: '/help',
+    name: 'Help',
+    component: HelpCenter,
   },
   // Add other routes as needed
 ]
